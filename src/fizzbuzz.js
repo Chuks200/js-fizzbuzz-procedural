aab4225ae05f664faee356ebae0f5e1c0ed1b217
Chuks200 <agbataekwechuks@gmail.com>
2024-05-20 22:03:34 +0100

    fizzbuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,9 +1,17 @@
 const answer = []
 
 // Write your code below this line
+// src/fizzbuzz.js
 
+for (let i = 1; i <= 15; i++) {
+    let output = '';
+  if (i % 3 === 0) output += 'Fizz'
+ if (i % 5 === 0) output += 'Buzz'
+    answer.push(output || i)
+}
 
-
+console.log(answer)
+module.exports = answer  // Assuming this is needed for the test setup
 
 
 // Don't touch the code below this line, we'll cover it later
